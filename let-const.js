9d774d925a71ec41a100fbedc706f05841af641a
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-02 20:37:33 +0600

    template add

---
+++ let-const.js
@@ -0,0 +1,14 @@
+// const name = "BD";
+// nam = "bd";
+// console.log(nam)
+
+
+// const color= ['red' , 'blue', 'green'];
+// color.push('pio');
+// console.log(color)
+
+function sum(a, b=5){
+    const total= a + b;
+    console.log(total)
+}
+sum(10)
---
+++ templates-tring.js
@@ -0,0 +1,4 @@
+let num1 = 30;
+let num2 = 10;
+const sum = `sum is ${num1 + num2}`;
+console.log(sum);
