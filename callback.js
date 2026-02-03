faf2f100817a7352a6e65717703ab8f91707350e
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-03 15:38:36 +0600

    callback recap

--- callback.js
+++ callback.js
@@ -1,23 +1,23 @@
-// const run = (spd, lngth) => {
-//   console.log("today i ran with", spd);
-//   setTimeout(() => {
-//     Callback();
-//   }, 3000);
-// };
-// const lngth = () => {
-//   console.log(" 5km");
-// };
-// run("dstnce", lngth);
-
-const missionResert = (topics, done) => {
-  console.log("Learning...", topics);
-
+const run = (spd, lngth) => {
+  console.log("today i ran with", spd);
   setTimeout(() => {
-    done();
+    lngth();
   }, 3000);
 };
-
-const done = () => {
-  console.log("Done in 2 days");
+const lngth = () => {
+  console.log(" 5km");
 };
-missionResert("JS", done);
+run("dstnce", lngth);
+
+// const missionResert = (topics, done) => {
+//   console.log("Learning...", topics);
+
+//   setTimeout(() => {
+//     done();
+//   }, 3000);
+// };
+
+// const done = () => {
+//   console.log("Done in 2 days");
+// };
+// missionResert("JS", done);
