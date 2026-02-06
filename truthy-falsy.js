6d712685e42ddaecb29b32b6f0be47c54cb64584
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-06 23:59:54 +0600

    logical not

--- truthy-falsy.js
+++ truthy-falsy.js
@@ -7,10 +7,27 @@ data = false;
 data = true;
 data = {};
 data = [];
+data = null;
+data = false;
+data = true;
+data = [];
 console.log("value of data", data);
 
 if (data) {
   console.log("value of data is truthy");
 } else {
   console.log(data, "is falsy");
+}
+
+// if i need to capture the falsy value to go inside if block
+// use logical not
+if (!data) {
+  console.log("inside if with a falsy value");
+}
+
+// capture all positive value
+// double not!!
+// capture any value to boolean(true/false)
+if (!!data === true) {
+  console.log("only true inside the double not");
 }
