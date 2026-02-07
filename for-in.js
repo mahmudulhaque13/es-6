43262fffa74ef126044b1921dc97a299f897de6d
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-07 20:51:47 +0600

    more for in

--- for-in.js
+++ for-in.js
@@ -10,14 +10,23 @@
 //   }
 // }
 
-const formData = {
-  username: "mahmud",
-  email: "test@gmail.com",
-  password: "1234",
-};
+// const formData = {
+//   username: "mahmud",
+//   email: "test@gmail.com",
+//   password: "1234",
+// };
 
-for (let field in formData) {
-  if (!formData[field]) {
-    console.log(field + " is required");
-  }
+// for (let field in formData) {
+//   if (!formData[field]) {
+//     console.log(field + " is required");
+//   }
+// }
+
+const book = {
+  title: "JS Basics",
+  price: 300,
+  author: "Mahmud",
+};
+for (let key in book) {
+  console.log(key + ":", book[key]);
 }
