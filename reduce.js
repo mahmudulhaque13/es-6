3ea9f8ab010ca24f11b0a53f7f9405ac11891708
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-12 20:09:32 +0600

    more reduce

--- reduce.js
+++ reduce.js
@@ -2,10 +2,21 @@
 // logic
 // }, initialValue);
 
-const numbers = [10, 70, 30, 40];
+// const numbers = [10, 70, 30, 40];
 
-const sum = numbers.reduce((acc, curr) => {
-  return acc + curr;
+// const sum = numbers.reduce((acc, curr) => {
+//   return acc + curr;
+// }, 0);
+
+// console.log(sum);
+
+const cart = [
+  { name: "Shirt", price: 600, qty: 2 },
+  { name: "Pant", price: 400, qty: 1 },
+];
+
+const totalPrice = cart.reduce((total, item) => {
+  return total + item.price * item.qty;
 }, 0);
 
-console.log(sum);
+console.log(totalPrice);
